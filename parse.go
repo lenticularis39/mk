553a9d835ace6137e95fa79b8b68507a6bc04254
Tomas Glozar <tglozar@gmail.com>
2022-01-02 15:20:28 +0100

    Change license to AGPLv3+

--- parse.go
+++ parse.go
@@ -1,3 +1,47 @@
+/*
+	Copyright (c) 2022 Tomas Glozar
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+	Copyright (c) 2013, Daniel C. Jones <dcjones@cs.washington.edu>
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	The views and conclusions contained in the software and documentation are those
+	of the authors and should not be interpreted as representing official policies,
+	either expressed or implied, of the FreeBSD Project.
+*/
+
 // This is a mkfile parser. It executes assignments and includes as it goes, and
 // collects a set of rules, which are returned as a ruleSet object.
 
@@ -113,7 +157,7 @@ func parsePipeInclude(p *parser, t token) parserStateFun {
 			p.basicErrorAtToken("empty pipe include", t)
 		}
 
-		args := make([]string, len(p.tokenbuf) + 1)
+		args := make([]string, len(p.tokenbuf)+1)
 		args[0] = "-c"
 		for i := 0; i < len(p.tokenbuf); i++ {
 			s := p.tokenbuf[i].val
@@ -121,7 +165,7 @@ func parsePipeInclude(p *parser, t token) parserStateFun {
 			if len(expanded) > 0 {
 				s = expanded[0]
 			}
-			args[i + 1] = s
+			args[i+1] = s
 		}
 
 		output, success := subprocess("sh", args, "", true)
